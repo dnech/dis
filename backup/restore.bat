20da7fbefc0915300c412aebda4be1aa070cb074
dnech <dnech@mail.ru>
2015-04-13 23:55:46 +0600

    Update Backup DB
    Add backup folder for all branch

--- backup/restore.bat
+++
@@ -1,3 +0,0 @@
-..\..\PostgreSQL\bin\pg_dump -c -C --column-inserts --serializable-deferrable -U postgres -h localhost -p 5432 test > arhiv\before_restore.sql
-..\..\PostgreSQL\bin\psql -U postgres test < dropall.sql
-..\..\PostgreSQL\bin\psql -U postgres < arhiv\db_last.sql
