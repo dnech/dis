daebd342d8b09bf33fefe0e9c0f9e60c5286ae5f
dnech <dnech@mail.ru>
2015-04-16 19:11:11 +0600

    Begin Ext Direct

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-var express = require('express'),
-	router = require('./app/router'),
-	http = require('http'),
-	path = require('path'),
-	uuid = require('node-uuid'),
-	crypto = require('crypto'),
-	Sequelize = require('sequelize');
+var express		= require('express'),
+	http		= require('http'),
+	path		= require('path'),
+	uuid		= require('node-uuid'),
+	crypto		= require('crypto'),
+	Sequelize	= require('sequelize'),
+	nconf		= require('nconf'),
+    extdirect	= require('extdirect'),
+	router		= require('./app/router');
 
-db = new Sequelize('test', 'postgres', '', {
-      dialect: "postgres", // or 'sqlite', 'mysql', 'mariadb'
-      port:    5432, // or 3306 (5432 for postgres)
+nconf.env().file({ file: './app/config.json'});
+
+db = new Sequelize(nconf.get("DB_NAME"), nconf.get("DB_USER"), nconf.get("DB_PASS"), {
+      dialect: nconf.get("DB_DIALECT"),   // or 'sqlite', 'mysql', 'mariadb'
+      port:    nconf.get("DB_PORT"),      // or 3306 (5432 for postgres)
 	  logging: function(log){
         //console.log(log);
       }
@@ -32,26 +36,47 @@ global.Sequelize = Sequelize;
 global.DB		 = db;
 global.UUID		 = uuid;
 global.Crypto	 = crypto;
-global.Salt		 = '2a4c13fb6c774b43486450065a732af8';
+global.Salt		 = nconf.get("SERVER_SALT");
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || nconf.get("SERVER_PORT"));
 //app.set('views', __dirname + '/views');
 //app.set('view engine', 'jade');
 //app.use(express.favicon());
 //app.use(express.logger('dev'));
+// handle the request for the api from the client
+
+/*          EXT DIRECT        */
+/* http://bannockburn.io/2013/06/ext-direct-with-sencha-touch-2-nodejs-expressjs/ */
+var EXTDIRECT_PATH = nconf.get("EXTDIRECT_PATH"),
+    EXTDIRECT_NAMESPACE = nconf.get("EXTDIRECT_NAMESPACE"),
+    EXTDIRECT_API_NAME = nconf.get("EXTDIRECT_API_NAME"),
+    EXTDIRECT_PREFIX = nconf.get("EXTDIRECT_PREFIX");
+	
+app.get('/directapi', function(request, response) {
+    var api = extdirect.getAPI(EXTDIRECT_NAMESPACE, EXTDIRECT_API_NAME, EXTDIRECT_PATH, EXTDIRECT_PREFIX);
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.end(api);
+});
+ 
+app.get(EXTDIRECT_PATH, function(request, response) {
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.end(JSON.stringify({success:false, msg:'Unsupported method. Use POST instead.'}));
+});
+ 
+app.post(EXTDIRECT_PATH, function(request, response) {
+    extdirect.processRoute(request, response, EXTDIRECT_PATH);
+});
+
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(global.PUBLIC));
 
-
-
-  
 //router
 router.init(app);
 
-app.listen(8080);
-console.log('Listening on port 8080');
+app.listen(nconf.get("SERVER_PORT"));
+console.log('Listening on port '+nconf.get("SERVER_PORT"));
---
+++ app/direct/DXServerDate.js
@@ -0,0 +1,6 @@
+var DXServerDate  = {
+    now: function(callback){
+        callback(new Date());
+    }
+}
+module.exports = DXServerDate;
